1bc77aa9e58999bb7231fc898cefd688ff16a719
qazxcdswe123 <v009008777@hotmail.com>
2017-12-04 22:38:49 +0800

    Update gaoji.sh

--- gaoji.sh
+++ gaoji.sh
@@ -6,5 +6,5 @@ read shuru #输入
 if [[ $shuru=1 ]]; then
   wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
 elif [[ $shuru=2 ]]; then
-  wget https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.1/tcp_nanqinlang_3.1.sh&&chmod +x tcp_nanqinlang_3.1.sh&&bash tcp_nanqinlang_3.1.sh
+  wget -N --no-check-certificate https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.1/tcp_nanqinlang_3.1.sh&&chmod +x tcp_nanqinlang_3.1.sh&&bash tcp_nanqinlang_3.1.sh
 fi
