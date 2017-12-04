01831f9a1a3fa60c71eac0737193199ca524eeb5
Neil-Chen <v3344521v@gmail.com>
2017-12-04 13:37:45 +0800

    1
    1

--- gaoji.sh
+++ gaoji.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #选择
 echo "1:ssr
-2bbr"
+2:bbr"
 read shuru #输入
 if [[ $shuru=1 ]]; then
-  echo:"hahahaha"
+  wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
 fi
