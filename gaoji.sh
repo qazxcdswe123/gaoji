127449ab64244cdda389bb1b69074f6107f90b15
Neil-Chen <v3344521v@gmail.com>
2017-12-04 13:47:26 +0800

    11
    1

--- gaoji.sh
+++ gaoji.sh
@@ -5,4 +5,6 @@ echo "1:ssr
 read shuru #输入
 if [[ $shuru=1 ]]; then
   wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+elif [[ $shuru=2 ]]; then
+  wget https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.1/tcp_nanqinlang_3.1.sh&&chmod +x tcp_nanqinlang_3.1.sh&&bash tcp_nanqinlang_3.1.sh
 fi
