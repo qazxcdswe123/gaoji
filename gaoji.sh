fcfb42b3df9e556db9301885045b5474a57071b7
Neil-Chen <v3344521v@gmail.com>
2017-12-05 13:13:22 +0800

    1
    1

--- gaoji.sh
+++ gaoji.sh
@@ -2,9 +2,12 @@
 #选择
 echo "1:ssr
 2:bbr"
-read shuru #输入
-if [[ $shuru=1 ]]; then
+read -p "请选择（输入数字序号）：" shuru #输入
+if [ "$shuru" == "" ]; then
+  echo "未选择序号，脚本退出"
+  exit 1
+if [[ "$shuru"=="1" ]]; then
   wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
-elif [[ $shuru=2 ]]; then
-  wget https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.1/tcp_nanqinlang_3.1.sh&&chmod +x tcp_nanqinlang_3.1.sh&&bash tcp_nanqinlang_3.1.sh
+elif [[ "$shuru"=="2" ]]; then
+  wget -N --no-check-certificate https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.1/tcp_nanqinlang_3.1.sh&&chmod +x tcp_nanqinlang_3.1.sh&&bash tcp_nanqinlang_3.1.sh
 fi
