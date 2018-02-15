45d9984c1829664ce94a25f26ddbc14263b31c1d
Neil <v009008777@hotmail.com>
2018-02-15 08:53:54 +0800

    fix -y

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update
-apt install sudo screen curl unzip
-apt upgrade
+apt install sudo screen curl unzip -y
+apt upgrade -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh&&chmod +x gaoji.sh&&bash gaoji.sh
