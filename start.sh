16f052160038e038b493c3985a6ebe75ff6d20d8
Neil <v009008777@hotmail.com>
2018-02-16 21:30:18 +0800

    rm upgrade

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt update
 apt install sudo screen curl unzip -y
-apt upgrade -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh&&chmod +x gaoji.sh&&bash gaoji.sh
