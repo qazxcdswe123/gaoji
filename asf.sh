7d4bf81a3d7c11440849ca6804ca098d2077e03c
qazxcdswe123 <v009008777@hotmail.com>
2018-02-17 20:48:44 +0800

    Update asf.sh

--- asf.sh
+++ asf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.0.4.3/ASF-linux-x64.zip
+wget https://github.com/JustArchi/ArchiSteamFarm/releases/latest/ASF-linux-x64.zip
 apt-get install libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev uuid-dev unzip screen -y
 unzip ASF-linux-x64.zip -d ASF/
 cd ASF/ 
