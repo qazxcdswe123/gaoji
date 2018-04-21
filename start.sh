b1f2d0f87c3343620975f3f6310c83f0a2ea924d
qazxcdswe123 <v009008777@hotmail.com>
2018-04-21 22:01:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt update
 apt install sudo screen curl unzip -y
+wget https://github.com/nanqinlang-tcp/tcp_nanqinlang/releases/download/3.4.2/tcp_nanqinlang-pro-3.4.2.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh&&chmod +x gaoji.sh&&bash gaoji.sh
