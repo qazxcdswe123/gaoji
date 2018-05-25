baa3021e239c5a273ce0ad521dc23fb072a69fd4
qazxcdswe123 <v009008777@hotmail.com>
2018-05-25 20:18:53 +0800

    Update asf.sh

--- asf.sh
+++ asf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-wget https://github.com/JustArchi/ArchiSteamFarm/releases/latest/ASF-linux-x64.zip
+wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.0.4.3/ASF-linux-x64.zip
 apt-get install libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev uuid-dev unzip screen -y
 unzip ASF-linux-x64.zip -d ASF/
 cd ASF/ 
 chmod +x ArchiSteamFarm
 echo -e "请用ftp工具将配置文件（json/2FA）上传"
-echo -e "退出请输入
-ctrl+c"
+echo -e "退出请输入 ctrl+c"
 ./ArchiSteamFarm
