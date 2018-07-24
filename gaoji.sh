14a66ee75da74eab04bbd992600ccef72454d1e6
Neil_chen <v009008777@hotmail.com>
2018-07-24 14:18:29 +0800

    add lamp
    Signed-off-by: Neil_chen <v009008777@hotmail.com>

--- gaoji.sh
+++ gaoji.sh
@@ -17,15 +17,15 @@ echo && echo -e "  你要做什么？
  ${Green_font_prefix}8.${Font_color_suffix} 一键安装V2ray
  ${Green_font_prefix}9.${Font_color_suffix} 安装Gdrive(谷歌网盘)
  
- 先使用screen!!!" && echo
+ 先使用screen!!!" &&
 echo -e "${Green_font_prefix} [安装前 请注意] ${Font_color_suffix}
+0. ${Green_font_prefix} 一定一定先screen ${Font_color_suffix}
 1. 若换内核时长时间卡住请Ctrl+c或者重装系统
 2. 本脚本仅支持 Debian / Ubuntu 系统更换内核，OpenVZ和Docker 不支持更换内核
 3. Debian 更换内核过程中会提示 [ 是否终止卸载内核 ] ，请选择 ${Green_font_prefix} NO ${Font_color_suffix}
 4. 安装BBR并重启服务器后，需要重新运行脚本 启动BBR
 5. 全部只在${Green_font_prefix} Debian8 ${Font_color_suffix}上测试过
-6. 不支持centos" && echo
-stty erase '^H' && read -p "(默认: 取消):" need
+6. 不支持centos" && echo stty erase '^H' && read -p "(默认: 取消):" need
 [[ -z "${need}" ]] && echo "已取消..." && exit 1
 if [[ ${need} == "1" ]]; then
 	wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh && chmod +x ssr.sh && bash ssr.sh
@@ -34,7 +34,14 @@ elif [[ ${need} == "2" ]]; then
 elif [[ ${need} == "3" ]]; then
 	wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/Fail2ban/master/fail2ban.sh && bash fail2ban.sh 2>&1 | tee fail2ban.log
 elif [[ ${need} == "4" ]]; then
-	wget -c http://soft.vpser.net/lnmp/lnmp1.4.tar.gz && tar zxf lnmp1.4.tar.gz && cd lnmp1.4 && ./install.sh lnmp
+echo -e "选择安装内容
+1. lnmp
+2. lamp" && echo stty erase '^H' && read -p "(默认: 取消):" web
+[[ -z "${web}" ]] && echo "已取消..." && exit 1
+if [[ ${web} == "1"]]; then
+	wget -c http://soft.vpser.net/lnmp/lnmp1.5.tar.gz && tar zxf lnmp1.5.tar.gz && cd lnmp1.5 && ./install.sh lnmp
+	elif [[ ${web} == "2"]]; then
+	apt-get -y install wget screen git && git clone https://github.com/teddysun/lamp.git && cd lamp && chmod +x *.sh && ./lamp.sh
 elif [[ ${need} == "5" ]]; then
 	wget https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/asf.sh && chmod +x asf.sh && bash asf.sh
 elif [[ ${need} == "6" ]]; then
