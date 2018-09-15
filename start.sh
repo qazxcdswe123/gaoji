2b293d53be92ee940a7f2aee3f07b2d7591a1731
qazxcdswe123 <v009008777@hotmail.com>
2018-09-15 21:03:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
 apt install sudo screen unzip -y
-apt install curl -y
+apt install curl libelf-dev -y
 wget https://github.com/tcp-nanqinlang/general/releases/download/3.4.5.1/tcp_nanqinlang-pro-3.4.5.1-nocheckvirt.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh&&chmod +x gaoji.sh&&bash gaoji.sh
