d4019bd5c648c2508b371bdca19c1c26176ad425
qazxcdswe123 <v009008777@hotmail.com>
2020-02-12 10:31:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ check_root() {
 apt update
 apt install sudo screen unzip curl libelf-dev -y
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh
-wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh && chmod +x gaoji.sh&&bash gaoji.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/qazxcdswe123/gaoji/master/gaoji.sh && chmod +x gaoji.sh && bash gaoji.sh
