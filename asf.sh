7400c5d93677788496df24acfa93ec99b9abc320
Neil-Chen <v009008777@hotmail.com>
2020-02-20 19:04:50 +0800

    fix myself

--- asf.sh
+++ asf.sh
@@ -4,7 +4,6 @@ export PATH
 
 Dependence="libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev uuid-dev unzip screen libkrb5-3"
 
-check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
 		release="centos"
 	elif cat /etc/issue | grep -q -E -i "debian"; then
@@ -21,8 +20,7 @@ check_sys(){
 		release="centos"
     fi
 	bit=`uname -m`
-}
-System_structure(){
+
     if [[ $(bit) == "x86_64" ]]; then
         target="x64"
     elif [[$(bit) == "arm-rbpi" ]]; then
@@ -31,15 +29,13 @@ System_structure(){
         echo -e "ASF不支持除x64和arm架构以外的系统，请重装系统吧"
     exit 1
     fi
-}
-Install_Dependence(){
+
     if [[ $(release) == "centos" ]]; then
         yum update && yum remove libssl1.0.0 && yum install $Dependence -y
     else
         apt-get update && apt-get purge libssl1.0.0 && apt-get install $Dependence -y
     fi
-}
-Get_ASF(){
+
     VER=$( wget -qO- https://github.com/JustArchiNET/ArchiSteamFarm/tags | grep -oE -m1 "/tag/v[^\"]*" | cut -dv -f2 )
     wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/$VER/ASF-linux-$target.zip
     unzip ASF-linux-x64.zip -d ASF/
@@ -48,4 +44,3 @@ Get_ASF(){
     echo -e "请用ftp工具将配置文件（json/2FA）上传"
     echo -e "滚完屏后再退出，退出请输入 ctrl+c"
     ./ArchiSteamFarm
-}
